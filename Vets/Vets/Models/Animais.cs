5f259b0535039f556aaa43b678917e99df845cab
Rodrigo <rodrigodomingospereira99@gmail.com>
2020-03-31 15:33:25 +0100

    Criação das classes que representam as tabelas

---
+++ Vets/Vets/Models/Animais.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vets.Models
+{
+    public class Animais{
+        public int ID{ get; set; }
+
+        public string Nome{ get; set; }
+
+        public string Especie{ get; set; }
+
+        public double Peso { get; set; }
+
+        public string Raca { get; set; }
+
+        public string Fotografia { get; set; }
+    }
+}
